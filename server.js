344be8fe17f12e19c077efecd1563df8265b95fa
cfrancisco726 <cfrancisco726@gmail.com>
2019-09-04 17:12:56 -0400

    modal heading title

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-'use strict';
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+
+const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, function() {
-	console.log('App web-server listening on port 3000');
+  console.log('App web-server listening on port 3000');
 });
--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -1,178 +1,164 @@
-'use strict';
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-	Panel,
-	Col,
-	Row,
-	Well,
-	Button,
-	ButtonGroup,
-	Label,
-	Modal
-} from 'react-bootstrap';
+import { Panel, Col, Row, Well, Button, ButtonGroup, Label, Modal } from 'react-bootstrap';
 import { bindActionCreators } from 'redux';
 import { deleteCartItem, updateCart, getCart } from '../../actions/cartActions';
 
 class Cart extends Component {
-	componentDidMount() {
-		this.props.getCart();
-	}
-	onDelete(_id) {
-		const currentRecordToDelete = this.props.cart;
+  componentDidMount() {
+    this.props.getCart();
+  }
 
-		const indexToDelete = currentRecordToDelete.findIndex(cart => {
-			return cart._id === _id;
-		});
-		let cartAfterDelete = [
-			...currentRecordToDelete.slice(0, indexToDelete),
-			...currentRecordToDelete.slice(indexToDelete + 1)
-		];
-		this.props.deleteCartItem(cartAfterDelete);
-	}
-	onIncrement(_id) {
-		this.props.updateCart(_id, 1, this.props.cart);
-	}
-	onDecrement(_id, quantity) {
-		if (quantity > 1) {
-			this.props.updateCart(_id, -1, this.props.cart);
-		}
-	}
+  onDelete(_id) {
+    const currentRecordToDelete = this.props.cart;
 
-	constructor(props, context) {
-		super(props, context);
+    const indexToDelete = currentRecordToDelete.findIndex(cart => {
+      return cart._id === _id;
+    });
+    const cartAfterDelete = [
+      ...currentRecordToDelete.slice(0, indexToDelete),
+      ...currentRecordToDelete.slice(indexToDelete + 1),
+    ];
+    this.props.deleteCartItem(cartAfterDelete);
+  }
 
-		this.handleShow = this.handleShow.bind(this);
-		this.handleClose = this.handleClose.bind(this);
+  onIncrement(_id) {
+    this.props.updateCart(_id, 1, this.props.cart);
+  }
 
-		this.state = {
-			show: false
-		};
-	}
+  onDecrement(_id, quantity) {
+    if (quantity > 1) {
+      this.props.updateCart(_id, -1, this.props.cart);
+    }
+  }
 
-	handleClose() {
-		this.setState({ show: false });
-	}
+  constructor(props, context) {
+    super(props, context);
 
-	handleShow() {
-		this.setState({ show: true });
-	}
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
 
-	render() {
-		if (this.props.cart[0]) {
-			return this.renderCart();
-		} else {
-			return this.renderEmpty();
-		}
-	}
+    this.state = {
+      show: false,
+    };
+  }
 
-	renderEmpty() {
-		return <div />;
-	}
+  handleClose() {
+    this.setState({ show: false });
+  }
 
-	renderCart() {
-		const cartItemsList = this.props.cart.map(function(cartArr) {
-			return (
-				<Panel key={cartArr._id}>
-					<Row style={{ margin: '20px' }}>
-						<Col xs={12} sm={4}>
-							<h6>{cartArr.artist}</h6>
-							<span> </span>
-						</Col>
-						<Col xs={12} sm={4}>
-							<h6>usd. {cartArr.price}</h6>
-						</Col>
-						<Col xs={12} sm={4}>
-							<h6>
-								qty. <Label bsStyle="success">{cartArr.quantity}</Label>
-							</h6>
-						</Col>
-						<Col xs={6} sm={4}>
-							<ButtonGroup style={{ minWidth: '300px' }}>
-								<Button
-									onClick={this.onDecrement.bind(
-										this,
-										cartArr._id,
-										cartArr.quantity
-									)}
-									bsStyle="default"
-									bsSize="small"
-								>
-									-
-								</Button>
-								<Button
-									onClick={this.onIncrement.bind(this, cartArr._id)}
-									bsStyle="default"
-									bsSize="small"
-								>
-									+
-								</Button>
-								<span> </span>
-								<Button
-									onClick={this.onDelete.bind(this, cartArr._id)}
-									bsStyle="danger"
-									bsSize="small"
-								>
-									DELETE
-								</Button>
-							</ButtonGroup>
-						</Col>
-					</Row>
-				</Panel>
-			);
-		}, this);
-		return (
-			<Panel header="Cart" bsStyle="primary">
-				{cartItemsList}
-				<Row style={{ margin: '20px' }}>
-					<Col xs={12}>
-						<h6>Total amount: {this.props.totalAmount}</h6>
-						<Button
-							onClick={this.handleShow.bind(this)}
-							Style="success"
-							bsSize="small"
-						>
-							Proceed to Checkout
-						</Button>
-					</Col>
-				</Row>
-				<Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
-					<Modal.Header closeButton>
-						<Modal.Title>Check out</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						<h6>Your order has been saved</h6>
-						<p>You will receive an email confirmation </p>
-					</Modal.Body>
-					<Modal.Footer>
-						<Col xs={6}>
-							<h6>total $:{this.props.totalAmount}</h6>
-						</Col>
-						<Button onClick={this.handleClose.bind(this)}>Close</Button>
-					</Modal.Footer>
-				</Modal>
-			</Panel>
-		);
-	}
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    if (this.props.cart[0]) {
+      return this.renderCart();
+    }
+    return this.renderEmpty();
+  }
+
+  renderEmpty() {
+    return <div />;
+  }
+
+  renderCart() {
+    const cartItemsList = this.props.cart.map(function(cartArr) {
+      return (
+        <Panel key={cartArr._id}>
+          <Row style={{ margin: '20px' }}>
+            <Col xs={12} sm={4}>
+              <h6>{cartArr.artist}</h6>
+              <span> </span>
+            </Col>
+            <Col xs={12} sm={4}>
+              <h6>usd. {cartArr.price}</h6>
+            </Col>
+            <Col xs={12} sm={4}>
+              <h6>
+                qty. <Label bsStyle="success">{cartArr.quantity}</Label>
+              </h6>
+            </Col>
+            <Col xs={6} sm={4}>
+              <ButtonGroup style={{ minWidth: '300px' }}>
+                <Button
+                  onClick={this.onDecrement.bind(this, cartArr._id, cartArr.quantity)}
+                  bsStyle="default"
+                  bsSize="small"
+                >
+                  -
+                </Button>
+                <Button
+                  onClick={this.onIncrement.bind(this, cartArr._id)}
+                  bsStyle="default"
+                  bsSize="small"
+                >
+                  +
+                </Button>
+                <span> </span>
+                <Button
+                  onClick={this.onDelete.bind(this, cartArr._id)}
+                  bsStyle="danger"
+                  bsSize="small"
+                >
+                  DELETE
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </Panel>
+      );
+    }, this);
+    return (
+      <Panel header="Cart" bsStyle="primary">
+        {cartItemsList}
+        <Row style={{ margin: '20px' }}>
+          <Col xs={12}>
+            <h6>Total amount: {this.props.totalAmount}</h6>
+            <Button onClick={this.handleShow.bind(this)} Style="success" bsSize="small">
+              Proceed to Checkout
+            </Button>
+          </Col>
+        </Row>
+        <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Check out</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h6>Your order has been saved</h6>
+            <p>You will receive an email confirmation </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Col xs={6}>
+              <h6>total $:{this.props.totalAmount}</h6>
+            </Col>
+            <Button onClick={this.handleClose.bind(this)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </Panel>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		cart: state.cart.cart,
-		totalAmount: state.cart.totalAmount
-	};
+  return {
+    cart: state.cart.cart,
+    totalAmount: state.cart.totalAmount,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators(
-		{
-			deleteCartItem: deleteCartItem,
-			updateCart: updateCart,
-			getCart: getCart
-		},
-		dispatch
-	);
+  return bindActionCreators(
+    {
+      deleteCartItem,
+      updateCart,
+      getCart,
+    },
+    dispatch
+  );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cart);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Cart);
